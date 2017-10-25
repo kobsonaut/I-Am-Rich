a28413d1fb9fbfc46e19e75c1256b8147e68b99d
Marcin Kobiela <kobsonauta@89-64-144-191.dynamic.chello.pl>
2017-10-25 15:52:12 +0200

    App with Motion Gesture Detection

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kobsonauta on 23.10.2017.
+//  Copyright © 2017 Kobsonauta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
